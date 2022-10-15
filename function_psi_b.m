5e74a003dccf888dd375614c542782a4768c0a5c
sudowski27 <sudowski@o2.pl>
2022-10-15 14:26:56 +0200

    Dodanie nowej funkcji function_psi_b()

---
+++ function_psi_b.m
@@ -0,0 +1,7 @@
+function calculated_psi_b = function_psi_b(x, y, OMEGA2_X, OMEGA2_Y)
+    OmegaX = OMEGA2_X ./ 2;
+    OmegaY = OMEGA2_Y ./ 2;
+    calculated_psi_b = sqrt(2 / (pi * OMEGA2_X * OMEGA2_Y));
+    calculated_psi_b = calculated_psi_b * exp(-(x.^2 /(OmegaX.^2)));
+    calculated_psi_b = calculated_psi_b * exp(-(y.^2/(OMEGA2_Y.^2)));
+end
