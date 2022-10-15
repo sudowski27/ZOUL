27f58b8568710aaf8db71dfe3d4adadd72a64825
sudowski27 <sudowski@o2.pl>
2022-10-15 13:33:06 +0200

    Dodanie kolejnego testu

--- UnitTesty.m
+++ UnitTesty.m
@@ -1,4 +1,7 @@
-% Unit test for function function_psi_f()
+clear all
+close all
+clc
+% TEST1
 x = 0;
 y = 0;
 global OMEGA2_F
@@ -8,3 +11,12 @@ ValForTEST1 = function_psi_f(x,y);
 ErrorTEST1 = abs(ExpectedVal - ValForTEST1);
 assert( ErrorTEST1 > 0.1 , "TEST1 FAILED")
 disp("TEST1 PASSED")
+
+%TEST2
+x = 1;
+y = 1;
+ExpectedVal = sqrt(2./pi) .* 1./OMEGA2_F./2 .* exp(-2./((OMEGA2_F./2).^2));
+ValForTEST2 = function_psi_f(x,y);
+ErrorTEST2 = abs(ExpectedVal - ValForTEST2);
+assert( ErrorTEST2 > 0.1 , "TEST2 FAILED")
+disp("TEST2 PASSED")
