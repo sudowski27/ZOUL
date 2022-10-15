1bb794c79d04bd766d77d3c73dec34afaf680ca6
sudowski27 <sudowski@o2.pl>
2022-10-15 13:46:03 +0200

    Zmiana z zmiennej globalnej na przesyłaną przez argument

--- UnitTesty.m
+++ UnitTesty.m
@@ -7,7 +7,7 @@ y = 0;
 global OMEGA2_F
 OMEGA2_F = 2.5*1e-6;
 ExpectedVal = sqrt(2/pi) * 1/OMEGA2_F/2;
-ValForTEST1 = function_psi_f(x,y);
+ValForTEST1 = function_psi_f(x,y, OMEGA2_F);
 ErrorTEST1 = abs(ExpectedVal - ValForTEST1);
 assert( ErrorTEST1 > 0.1 , "TEST1 FAILED")
 disp("TEST1 PASSED")
@@ -16,7 +16,7 @@ disp("TEST1 PASSED")
 x = 1;
 y = 1;
 ExpectedVal = sqrt(2./pi) .* 1./OMEGA2_F./2 .* exp(-2./((OMEGA2_F./2).^2));
-ValForTEST2 = function_psi_f(x,y);
+ValForTEST2 = function_psi_f(x, y, OMEGA2_F);
 ErrorTEST2 = abs(ExpectedVal - ValForTEST2);
 assert( ErrorTEST2 > 0.1 , "TEST2 FAILED")
 disp("TEST2 PASSED")
--- function_psi_f.m
+++ function_psi_f.m
@@ -1,7 +1,6 @@
-function calculated_psi_f = function_psi_f(x,y)
-    global OMEGA2_F
-    Omega_F = OMEGA2_F.*2;
+function calculated_psi_f = function_psi_f(x, y, OMEGA2_F)
     calculated_psi_f = sqrt(2./pi); % 1
+    Omega_F = OMEGA2_F / 2;
     calculated_psi_f = calculated_psi_f ./ Omega_F;
 
     ExpForCalculating = exp(-(x.^2 + y.^2))./(Omega_F.^2);
