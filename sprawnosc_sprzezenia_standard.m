c8254461834519ab0a28f51c1b95e0ff392d00e2
sudowski27 <sudowski@o2.pl>
2022-10-15 12:31:52 +0200

    Dodanie zmiennej globalnej

--- sprawnosc_sprzezenia_standard.m
+++ sprawnosc_sprzezenia_standard.m
@@ -1,2 +1,8 @@
 Lambda = 0.85; % [um]
 MetricPrefixLambda = 1e-6;
+
+% GLOBAL VARIABLES
+global OMEGA2_F
+OMEGA2_F = 2.5; % [um]
+MetricPrefixOMEGA2_F = 1e-6;
+OMEGA2_F = OMEGA2_F * MetricPrefixOMEGA2_F;
