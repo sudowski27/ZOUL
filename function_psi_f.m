191b5982e042850b6b9af79e826998fa3a76f2c6
sudowski27 <sudowski@o2.pl>
2022-10-26 18:44:29 +0200

    Kosmetyka

--- function_psi_f.m
+++ function_psi_f.m
@@ -1,7 +1,7 @@
 function calculated_psi_f = function_psi_f(x, y, OMEGA2_F)
     calculated_psi_f = sqrt(2./pi); % 1
-    Omega_F = OMEGA2_F / 2;
-    calculated_psi_f = calculated_psi_f * 1/Omega_F;
+    Omega_F = OMEGA2_F ./ 2;
+    calculated_psi_f = calculated_psi_f * 1./Omega_F;
     ExpForCalculating = exp((-(x.^2 + y.^2))./(Omega_F.^2));
     calculated_psi_f = calculated_psi_f .* ExpForCalculating;
 end
