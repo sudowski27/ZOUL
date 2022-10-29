0a9621e7e20a6c62e19fbf1ee30cb3ee5b9921e0
sudowski27 <sudowski@o2.pl>
2022-10-29 13:06:49 +0200

    Modyfikacja pliku test_plots.m

--- test_plots.m
+++ test_plots.m
@@ -1,8 +1,6 @@
 T2 = 0.8;
 omega_samples = 0:1e-4:2*pi/T2;
 
-H_0_samples = zeros(1, length(omega_samples));
-
-H_0_samples = Nadajnik(omega_samples, 0.6);
+H_0_samples = Transmitter(omega_samples, 0.6);
 
 plot(omega_samples, H_0_samples);
