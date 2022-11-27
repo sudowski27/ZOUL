64e30c244d6a68daa312da955018e66b45e1fe7e
sudowski27 <sudowski@o2.pl>
2022-11-27 21:21:38 +0100

    Dodanie funkcji g_0_first_integral_in_numerator(), która oblicza pierwszą całkę w liczniku dla obliczenia g_0. Korzysta ona z innej funkcji, która oblicza sam argument dla całki

---
+++ g_0_first_integral_in_numerator.m
@@ -0,0 +1,8 @@
+function integral = g_0_first_integral_in_numerator(alpha_s, L_1, r_1, r_2, alpha_l2, L_2, z, alpha_l1)
+	zmin = 0;
+	zmax = L_1;
+	fun = @(z) arg_for_g_0_first_integral_in_numerator(alpha_s, L_1, r_1, r_2, alpha_l2, L_2, z, alpha_l1);
+
+	bare_integral = integral(fun, zmin, zmax);
+	integral = 2 .* bare_integral;
+end
