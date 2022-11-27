1e930e3edfbea9b3c6926baf1c5fb58d2d633b9c
sudowski27 <sudowski@o2.pl>
2022-11-27 22:30:35 +0100

    Dodanie mechaniki do funkcji g_0_second_integral_in_numerator(), która służy do obliczenia finalnej drugiej całki w liczniku ułamka, który oblicza wartość g_0

---
+++ g_0_second_integral_in_numerator.m
@@ -0,0 +1,8 @@
+function integral = g_0_second_integral_in_numerator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2, z, L)
+	zmin = L_1;
+	zmax = L;
+	fun = @(z) arg_for_g_0_second_integral_in_numerator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2, z);
+
+	bare_integral = integral(fun, zmin, zmax);
+	integral = 2 .* bare_integral;
+end
