742555ae44ac39fac7f460727f67ef764b85d6d1
sudowski27 <sudowski@o2.pl>
2022-11-27 15:05:04 +0100

    Edycja funkcji C_function() : dodanie argumentów oraz logiki :: funkcja ma za zadanie obliczyć całkę

--- C_function.m
+++ C_function.m
@@ -1,3 +1,8 @@
-function C = C_function()
-	
+function C = C_function(alpha_s, L1, r_1, r_2, alpha_l2, L2, z, P_out_slash_P_sg, Beta, L)
+	zmin = L1
+	zmax = L;
+
+	fun = @(z) arg_for_C_integral_function(alpha_s, L1, r_1, r_2, alpha_l2, L2, z, P_out_slash_P_sg, Beta);
+
+	C = integral(fun, zmin, zmax);
 end
