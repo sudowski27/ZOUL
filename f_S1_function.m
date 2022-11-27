92469388f0d225344c456d43ce0295d168bacc01
sudowski27 <sudowski@o2.pl>
2022-11-27 21:20:53 +0100

    Zamiana słowa lambda na gamma w pliku obliczającym f_S1

--- f_S1_function.m
+++ f_S1_function.m
@@ -1,7 +1,7 @@
-function f_S1 = f_S1_function(lambda_1, z, r_2)
+function f_S1 = f_S1_function(gamma_1, z, r_2)
 	fraction_part = 1 ./ r_2;
 
-	exp_part = exp(-lambda_1 .* z);
+	exp_part = exp(-gamma_1 .* z);
 
 	f_S1 = fraction_part .* exp_part;
 end
