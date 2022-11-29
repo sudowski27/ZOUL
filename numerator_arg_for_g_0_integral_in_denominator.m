e9bd19a0dad3b1beb9d04159cba95acf7681ea8b
sudowski27 <sudowski@o2.pl>
2022-11-30 00:03:07 +0100

    Dodanie mechaniki obliczania licznika dla całki

--- numerator_arg_for_g_0_integral_in_denominator.m
+++ numerator_arg_for_g_0_integral_in_denominator.m
@@ -1,3 +1,12 @@
-function arg = numerator_arg_for_g_0_integral_in_denominator()
+function arg = numerator_arg_for_g_0_integral_in_denominator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2, z)
+	gamma_1 = gamma_1_function(L_1, r_1, r_2, alpha_s, alpha_l2, L_2);
+	f_r1 = f_R1_function(gamma_1, z);
+	abs_f_r1 = abs(f_r1);
+	square_of_abs_f_r1 = abs_f_r1 .^ 2;
 
+	f_S1 = f_S1_function(gamma_1, z, r_2);
+	abs_f_S1 = abs(f_S1);
+	square_of_abs_f_S1 = abs_f_S1 .^ 2;
+
+	arg = square_of_abs_f_r1 + square_of_abs_f_S1;
 end
