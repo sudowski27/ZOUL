1c14770f927b60dca405d7d998f2c1661ccf30e3
sudowski27 <sudowski@o2.pl>
2022-12-02 01:56:30 +0100

    Zmiana nazwy zwracanej wartości przez funkcję

--- g_0_first_integral_in_numerator.m
+++ g_0_first_integral_in_numerator.m
@@ -4,5 +4,5 @@ function integral_ans = g_0_first_integral_in_numerator(alpha_s, L_1, r_1, r_2, 
 	fun = @(z) arg_for_g_0_first_integral_in_numerator(alpha_s, L_1, r_1, r_2, alpha_l2, L_2, z, alpha_l1);
 
 	bare_integral = integral(fun, zmin, zmax);
-	integral = 2 .* bare_integral;
+	integral_ans = 2 .* bare_integral;
 end
