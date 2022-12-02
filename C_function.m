a1c7be2352dca977229079574ce92ef1c180b60c
sudowski27 <sudowski@o2.pl>
2022-12-02 01:51:43 +0100

    Redefinicja argimentów i dodanie średnika po zmin

--- C_function.m
+++ C_function.m
@@ -1,5 +1,5 @@
-function C = C_function(alpha_s, L1, r_1, r_2, alpha_l2, L2, z, P_out_slash_P_sg, Beta, L)
-	zmin = L1
+function C = C_function(alpha_s, L1, r_1, r_2, alpha_l2, L2, P_out_slash_P_sg, Beta, L)
+	zmin = L1;
 	zmax = L;
 
 	fun = @(z) arg_for_C_integral_function(alpha_s, L1, r_1, r_2, alpha_l2, L2, z, P_out_slash_P_sg, Beta);
