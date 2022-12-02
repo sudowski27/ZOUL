4c30fed5e71a56505015b5c815c84da59fb378b6
sudowski27 <sudowski@o2.pl>
2022-12-02 02:03:44 +0100

    Zmiana argumentów w definicji funkcji

--- g_0_second_integral_in_numerator.m
+++ g_0_second_integral_in_numerator.m
@@ -1,4 +1,4 @@
-function integral = g_0_second_integral_in_numerator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2, z, L)
+function integral_ans = g_0_second_integral_in_numerator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2, L)
 	zmin = L_1;
 	zmax = L;
 	fun = @(z) arg_for_g_0_second_integral_in_numerator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2, z);
