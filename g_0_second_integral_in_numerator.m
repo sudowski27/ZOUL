b8aa74b4d010c875bd85a82af2e6d25350d5be58
sudowski27 <sudowski@o2.pl>
2022-12-02 02:04:37 +0100

    Zmiana zwracanej wartości z integral na integral_ans

--- g_0_second_integral_in_numerator.m
+++ g_0_second_integral_in_numerator.m
@@ -4,5 +4,5 @@ function integral_ans = g_0_second_integral_in_numerator(L_1, r_1, r_2, alpha_s,
 	fun = @(z) arg_for_g_0_second_integral_in_numerator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2, z);
 
 	bare_integral = integral(fun, zmin, zmax);
-	integral = 2 .* bare_integral;
+	integral_ans = 2 .* bare_integral;
 end
