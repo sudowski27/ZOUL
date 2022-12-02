1b08586d8169d561297df5ff311f545cefa96af1
sudowski27 <sudowski@o2.pl>
2022-12-02 01:54:26 +0100

    redefinicja argumentów i zwracanej nawzwy zmiennej

--- g_0_denominator.m
+++ g_0_denominator.m
@@ -1,4 +1,4 @@
-function integral = g_0_denominator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2, z, P_out_slash_P_sg)
+function integral_ans = g_0_denominator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2,P_out_slash_P_sg)
 	zmin = 0;
 	zmax = L_1;
 	fun = @(z) arg_for_g_0_integral_in_denominator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2, z, P_out_slash_P_sg);
