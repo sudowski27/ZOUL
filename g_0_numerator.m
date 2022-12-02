edaf29b983e7e113bdd0fe600a5b4e891d1cccef
sudowski27 <sudowski@o2.pl>
2022-12-02 02:00:28 +0100

    Zmniejszenie argumentów  w definicji funkcji g_0_numerator

--- g_0_numerator.m
+++ g_0_numerator.m
@@ -1,4 +1,4 @@
-function ans = g_0_numerator(r_1, r_2, alpha_s, L_1, r_1, alpha_l2, L_2, z, alpha_l1, L, P_out_slash_P_sg, Beta)
+function ans = g_0_numerator(r_1, r_2, alpha_s, L_1, alpha_l2, L_2,alpha_l1, L, P_out_slash_P_sg, Beta)
 	ans = N_1_function(r_1, r_2);
 	ans = ans + g_0_first_integral_in_numerator(alpha_s, L_1, r_1, r_2, alpha_l2, L_2, z, alpha_l1);
 	ans = ans + g_0_second_integral_in_numerator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2, z, L);
