8256f05dff0eab5d06d1ba9a49cf3019f89ebcb9
sudowski27 <sudowski@o2.pl>
2022-12-02 02:01:21 +0100

    Zmiana argumentów podczas wywołania metody g_0_first_integral_in_numerator

--- g_0_numerator.m
+++ g_0_numerator.m
@@ -1,6 +1,6 @@
 function ans = g_0_numerator(r_1, r_2, alpha_s, L_1, alpha_l2, L_2,alpha_l1, L, P_out_slash_P_sg, Beta)
 	ans = N_1_function(r_1, r_2);
-	ans = ans + g_0_first_integral_in_numerator(alpha_s, L_1, r_1, r_2, alpha_l2, L_2, z, alpha_l1);
 	ans = ans + g_0_second_integral_in_numerator(L_1, r_1, r_2, alpha_s, alpha_l2, L_2, z, L);
 	ans = ans + C_function(alpha_s, L_1, r_1, r_2, alpha_l2, L_2, z, P_out_slash_P_sg, Beta, L);
+	ans = ans + g_0_first_integral_in_numerator(alpha_s, L_1, r_1, r_2, alpha_l2, L_2,alpha_l1);
 end
