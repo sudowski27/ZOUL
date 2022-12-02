3f10c481c3c8528f7b786680d186fc553a37d3e8
sudowski27 <sudowski@o2.pl>
2022-12-02 01:55:34 +0100

    Zmiana argumentów pobieranych przez funkcję

--- g_0_first_integral_in_numerator.m
+++ g_0_first_integral_in_numerator.m
@@ -1,4 +1,4 @@
-function integral = g_0_first_integral_in_numerator(alpha_s, L_1, r_1, r_2, alpha_l2, L_2, z, alpha_l1)
+function integral_ans = g_0_first_integral_in_numerator(alpha_s, L_1, r_1, r_2, alpha_l2, L_2, alpha_l1)
 	zmin = 0;
 	zmax = L_1;
 	fun = @(z) arg_for_g_0_first_integral_in_numerator(alpha_s, L_1, r_1, r_2, alpha_l2, L_2, z, alpha_l1);
