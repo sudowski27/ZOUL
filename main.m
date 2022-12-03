50a09267b7ee612f92c468b4459c42ff646c1987
sudowski27 <sudowski@o2.pl>
2022-12-03 18:26:36 +0100

    Dodanie wykresu 5

--- main.m
+++ main.m
@@ -202,3 +202,53 @@ title({str_for_title_1;
     str_for_title_7});
 xlabel('g_0L')
 ylabel("P_{out} / P_{sg}")
+
+% Wykres 5
+number_of_figure = 5;
+Pout_Psg_ratio = 0:0.001:0.4;
+r_2 = 0.9;  % 7 [cm]
+L = 12e-2;  % 12 [cm]
+
+L_1(1)=2e-2;
+L_1(2)=3e-2;
+L_1(3)=5e-2;
+L_1(4)=7e-2;
+
+figure(number_of_figure)
+% Inicjacja tablic zerami
+g_0 = zeros(1, length(Pout_Psg_ratio));
+g_0L = zeros(1, length(Pout_Psg_ratio));
+
+for i = 1:length(L_1)
+	for j = 1:length(Pout_Psg_ratio)
+        Beta = 5;
+        alpha_s = 1 ./ L_2;
+        alpha_2l = 0.1 ./ L_2;
+        alpha_1l = 0.1 ./ L_1(i);
+        L_2 = L - L_1(i);
+        r_1 = 1;
+
+		g_0(j) = g_0_function(r_1, r_2, alpha_s, L_1(i), alpha_2l, L_2, alpha_1l, L, Pout_Psg_ratio(j), Beta);
+        g_0L(j) = g_0(j) .* L;
+	end
+    plot(g_0L, Pout_Psg_ratio,'LineWidth', 2)
+    hold on
+end
+
+legend('L_1 = 2cm', 'L_1 = 3cm','L_1 = 5cm','L_1 = 7cm')
+str_for_title_1 = 'Wykres 5';
+str_for_title_2 = 'Zmiana parametru L_1';
+str_for_title_3 = '\beta=5';
+str_for_title_4 = "\alpha_{s}=1/L_2";
+str_for_title_5 = "\alpha_{l2}=0.1/L_{2}";
+str_for_title_6 = "\alpha_{l1}=0.1/L_1";
+str_for_title_7 = "r_2=0.9";
+title({str_for_title_1;
+    str_for_title_2;
+    str_for_title_3;
+    str_for_title_4;
+    str_for_title_5;
+    str_for_title_6;
+    str_for_title_7});
+xlabel('g_0L')
+ylabel("P_{out} / P_{sg}")
